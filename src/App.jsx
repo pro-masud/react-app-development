97292d494402a12dda158a773d0c46eac8f793fe
pro-mannan <promasudbd@gmail.com>
2023-11-17 13:00:08 +0600

    handling button system working successfully complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 // import HeadingCSS from './assets/app.module.css';
 function App() {
+  const  handleButton = () => {
+    alert('Hello World');
+  };
+
   return (
     <>
-      <button onClick={() => alert("One Click")} style={{padding: "20px", backgroundColor:'#000', color:'red', border:"none"}}>Click Me</button>
-      <button onDoubleClick={() => alert("double Click")} style={{padding: "20px", backgroundColor:'#000', color:'red', border:"none"}}>Click Me</button>
+      <button onClick={() => handleButton()} style={{padding: "20px", backgroundColor:'#000', color:'red', border:"none"}}>Click Me</button>
     </>
   );
 }
